23941c234003921b6627068a2d1d270eae357299
Jamil Mariano <marianojamil15@gmail.com>
2024-07-05 17:24:36 +0800

    added videos functionality scroll

--- javaScript.js
+++ javaScript.js
@@ -19,3 +19,18 @@ function position(id){
   // card.style.transform = 'scale(1.5)';
   console.log(id)
 }
+
+// Smooth scrolling for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          target.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
